c10ca27163e056beba8543733fc2ec16b9125620
Jonathan0827 <limjunehyeop@gmail.com>
2025-11-09 15:13:01 +0900

    Initial Commit

---
+++ EFB/EFBApp.swift
@@ -0,0 +1,17 @@
+//
+//  EFBApp.swift
+//  EFB
+//
+//  Created by Jonathan Lim on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct EFBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
