9063c11a5c29808a976409c31898311504f61c21
Jonathan0827 <limjunehyeop@gmail.com>
2025-11-15 20:34:05 +0900

    Fix airport data

--- EFB/airportInfo.swift
+++ EFB/airportInfo.swift
@@ -14,19 +14,24 @@ func airportInfo(_ icao: String, completion: @escaping (AirportDetail) -> Void) 
         
     } else {
         AF.request("https://airportdb.io/api/v1/airport/\(icao)?apiToken=\(apiKey!)")
+            .response { r in
+                if let data = r.data {
+//                    print(String(data: data, encoding: .utf8) ?? "no data")
+                }
+            }
             .responseDecodable(of: AirportDetail.self) { res in
                 switch res.result {
                 case .success(let value):
-                    print(value)
+//                    print(value)
 //                    print(value.icaoCode!)
 //                    print(value.name)
 //                    print(value.type)
 //                    print(value.iataCode ?? "no iata")
 //                    print(value.country.name)
 //                    print(value.region.name)
-                    for rwy in value.runways {
-                        print(rwy.leIdent, rwy.heIdent, rwy.lengthFt, rwy.widthFt)
-                    }
+//                    for rwy in value.runways {
+//                        print(rwy.leIdent, rwy.heIdent, rwy.lengthFt, rwy.widthFt)
+//                    }
                     completion(value)
                 case .failure(let error):
                     print(error)
